0ca2ec445579142f06c94c287684e458a387416d
Davy de Souza Assunção <davydesouzabar@gmail.com>
2022-02-11 17:16:06 -0400

    :sparkles: First commit

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: "knex_test",
+      user: "postgres",
+      password: "19873422189"
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+};
